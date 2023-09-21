aeefe2032d3416e1d0f052425b1a9667718369a6
Matheus438 <matheusbispoa3@gmail.com>
2023-09-21 15:17:38 -0300

    commit da listagem

--- src/components/BuscaCepForm.tsx
+++ src/components/BuscaCepForm.tsx
@@ -1,53 +1,59 @@
 import React, { useState, ChangeEvent, FormEvent, useEffect } from "react";
 
 import styles from "../App.module.css"
+import Footer from "./Footer";
+import Header from "./Header";
 
 const BuscaCepForm = () => {
 
-    const [cep, setCep] = useState<string>(""); 
+    const [cep, setCep] = useState<string>("");
     const [localidade, setLocalidade] = useState<string>("");
     const [uf, setUf] = useState<string>("");
     const [erro, setErro] = useState<string>("");
-    const findCep = (e:FormEvent) => {
-    
+    const findCep = (e: FormEvent) => {
+
         e.preventDefault();
-        
-        fetch('https://viacep.com.br/ws/'+cep+'/json',
-        {
-            method: 'GET'
-        }).then(response => response.json())
-        .then(
-            data => {
-                setLocalidade(data.localidade);
-                setCep(data.cep);
-                setUf(data.uf);
-                setErro("")
-            }
-        ).catch(error => {
-            setErro("Pesquisa Inválida");
-        });
+
+        fetch('https://viacep.com.br/ws/' + cep + '/json',
+            {
+                method: 'GET'
+            }).then(response => response.json())
+            .then(
+                data => {
+                    setLocalidade(data.localidade);
+                    setCep(data.cep);
+                    setUf(data.uf);
+                    setErro("")
+                }
+            ).catch(error => {
+                setErro("Pesquisa Inválida");
+            });
 
         console.log("Localidade:" + localidade);
     }
 
-    const submitForm =  (e: ChangeEvent<HTMLInputElement>) => {
-        if(e.target.name === "cep"){
+    const submitForm = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.name === "cep") {
             setCep(e.target.value);
         }
     }
- 
+
     return (
         <div>
-            <form onSubmit={findCep}>
-                <label htmlFor="cep">CEP</label>
-                <input type="text" name="cep" id="cep" onChange={submitForm} />
-                <input type="submit" value="Pesquisar" />
-            </form>
-            
-            <p>Cidade:  {localidade}</p> 
-            <p>Estado:  {uf}</p> 
-            <p>CEP      {cep}</p>    
-            <p className={styles.error}>e{erro}</p> 
+            <Header />
+            <main className={styles.main}>
+                <form onSubmit={findCep}>
+                    <label htmlFor="cep">CEP</label>
+                    <input type="text" name="cep" id="cep" onChange={submitForm} />
+                    <input type="submit" value="Pesquisar" />
+                </form>
+
+                <p>Cidade:  {localidade}</p>
+                <p>Estado:  {uf}</p>
+                <p>CEP      {cep}</p>
+                <p className={styles.error}>e{erro}</p>
+            </main>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Cadastro.tsx
@@ -0,0 +1,11 @@
+import { defaultMaxListeners } from 'events';
+import React,{Component, useState, ChangeEvent, FormEvent, useEffect
+} from 'react';
+const Cadastro = ( ) => {
+    return(
+        <div>
+            <h1>Pagina de Cadastro</h1>
+        </div>
+    );
+}
+export default Cadastro;
