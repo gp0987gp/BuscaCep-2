b51241b818374b9a4952ea2d555cb6d7c2e2701e
Matheus438 <matheusbispoa3@gmail.com>
2023-10-19 10:19:32 -0300

    buscacep

--- src/components/Listagem.tsx
+++ src/components/Listagem.tsx
@@ -7,46 +7,100 @@ import { CadastroInterface } from '../interface/CadastroInterface';
 const Listagem = () => {
 
     const [usuarios, setUsuarios] = useState<CadastroInterface[]>([]);
+    const [pesquisa, setPesquisa] = useState<string>('')
     const [error, setError] = useState("");
 
-    useEffect(() => {
-        async function fatchData() {
+    const handleState = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.name === "pesquisa") {
+            setPesquisa(e.target.value);
+        }
+    }
+
+    const buscar = (e: FormEvent) => {
+        e.preventDefault();
+
+        async function fetchData() {
             try {
-                const response = await axios.get('http://10.137.9.131:8000/api/find')
-                setUsuarios(response.data.data);
+                const response = await axios.post('http://10.137.9.134:8000/api/findNome',
+                    { nome: pesquisa },
+                    {
+                        headers: {
+                            "Accept": "application/json",
+                            "Content-Type": "application/json"
+                        }
+                    }).then(function (response) {
+                        setUsuarios(response.data.data);
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+            
 
             } catch (error) {
-                setError("Ocorreu um erro");
-                console.log(error);
-            }
+            console.log(error);
         }
+    }
+    fetchData();
 
-        fatchData();
-    }, []);
+}
+
+useEffect(() => {
+    async function fatchData() {
+        try {
+            const response = await axios.get('http://10.137.9.134:8000/api/find')
+            setUsuarios(response.data.data);
+
+        } catch (error) {
+            setError("Ocorreu um erro");
+            console.log(error);
+        }
+    }
+
+    fatchData();
+}, []);
 
 
-    return (
-        <div>
-            <main className={styles.main}>
-                <div className='container'>
+return (
+    <div>
+        <main className={styles.main}>
+            <div className='container'>
+
+                <div className='col-md mb-3'>
                     <div className='card'>
                         <div className='card-body'>
-                            <h5 className='card-title'>
-                                Listagem de Usuários
-                            </h5>
-                            <table className='table table-hover '>
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Nome</th>
-                                        <th>CPF</th>
-                                        <th>E-mail</th>
-                                        <th>Ações</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {usuarios.map(usuario => (
-                                        <tr key={usuario.id}>
+                            <h5 className='card-title'>Pesquisar</h5>
+                            <form onSubmit={buscar} className='row'>
+                                <div className='col-10'>
+                                    <input type="text" name="pesquisa"
+                                        className='form-control'
+                                        onChange={handleState} />
+                                </div>
+                                <div className='col-1'>
+                                    <button type='submit' className='btn btn-success'>Pesquisar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='card'>
+                    <div className='card-body'>
+                        <h5 className='card-title'>
+                            Listagem de Usuários
+                        </h5>
+                        <table className='table table-hover '>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nome</th>
+                                    <th>CPF</th>
+                                    <th>E-mail</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {usuarios.map(usuario => (
+                                    <tr key={usuario.id}>
                                         <td>{usuario.id}</td>
                                         <td>{usuario.nome}</td>
                                         <td>{usuario.cpf}</td>
@@ -56,14 +110,14 @@ const Listagem = () => {
                                             <a href="#" className='btn btn-danger btn-sm'>Excluir</a>
                                         </td>
                                     </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </main>
-        </div>
-    );
+            </div>
+        </main>
+    </div>
+);
 }
 export default Listagem;
